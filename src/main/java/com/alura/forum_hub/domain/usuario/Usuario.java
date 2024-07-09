53a0a9e78e4e9257f4d01cd966a988c8de9f9766
Eridan Paz <eribpaz@gmail.com>
2024-07-09 10:04:19 -0300

    [feat]: Início do projeto.

---
+++ src/main/java/com/alura/forum_hub/domain/usuario/Usuario.java
@@ -0,0 +1,9 @@
+package com.alura.forum_hub.domain.usuario;
+
+public class Usuario {
+  private Long id;
+  private String nome;
+  private String email;
+  private String senha;
+  private String perfil;
+}
